6a3a77d07ca31ec748cf518e61ab0ad54ccc4b91
Tommaso Costantini <info@tommasocostantini.it>
2017-09-15 11:35:20 +0200

    Add new task for PHP and JS file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 // Create Variables
-var gulp 			= require('gulp');
-var $ 				= require('gulp-load-plugins')();
-var runSequence 	= require('run-sequence');
-var mainBowerFiles 	= require('main-bower-files');
-var bs 				= require("browser-sync").create();
+var gulp 			          = require('gulp');
+var $ 				          = require('gulp-load-plugins')();
+var runSequence 	      = require('run-sequence');
+var mainBowerFiles 	    = require('main-bower-files');
+var browserSync 				= require("browser-sync").create();
 
-// Task for SASS files. In this task the files are minimized and saved in build/css folder with .css extension
+// Task for SASS files.
 gulp.task('sass', function () {
 	return gulp.src('scss/**/*.scss')
 	.pipe($.sass().on('error', $.sass.logError))
@@ -15,16 +15,29 @@ gulp.task('sass', function () {
 	}))
 	.pipe($.cleanCss({compatibility: 'ie8'}))
 	.pipe(gulp.dest('../css'))
-	.pipe(bs.stream());
+	.pipe(browserSync.stream());
 });
 
-// Task for BOWER files. In this task the files are saved in app/assets/vendor
+gulp.task('php', function() {
+  return gulp.src('../**/*.php')
+  .pipe(browserSync.stream());
+})
+
+// Task for JS files.
+gulp.task('js', function() {
+  return gulp.src('js/**/*.js')
+  .pipe($.uglify())
+  .pipe(gulp.dest('../js'))
+  .pipe(browserSync.stream());
+});
+
+// Task for BOWER files. 
 gulp.task('bower', function() {
     return gulp.src(mainBowerFiles(), {base: 'bower_components'})
         .pipe(gulp.dest('vendor'));
 });
 
-// Task for VENDOR JS files. In this task the files are minimized and saved in build/js folder
+// Task for VENDOR JS files. 
 gulp.task('vendor-js', () => {
   return gulp.src(['vendor/jquery/**/*.js', 'vendor/**/*.js'])
       .pipe($.concat('vendor.js'))
@@ -32,7 +45,7 @@ gulp.task('vendor-js', () => {
       .pipe(gulp.dest('../js'))
 });
 
-// Task for VENDOR CSS files. In this task the files are minimized and saved in build/css folder
+// Task for VENDOR CSS files.
 gulp.task('vendor-css', () => {
   return gulp.src('vendor/**/*.css')
       .pipe($.concat('vendor.css'))
@@ -47,10 +60,11 @@ gulp.task('inject', () => {
 
 // Task SERVE. Init Browser Sync
 gulp.task('serve', function() {
-	bs.init({
-		proxy: "wpgulp.dev"
+	browserSync.init({
+    // Your local server name
+		// proxy: "example.dev"
 	})
 	gulp.watch("scss/**/*.scss", ['sass'])
-	// gulp.watch("/js/**/*.js", ['js'])
-	gulp.watch("../**/*.php")
+	gulp.watch("js/**/*.js", ['js'])
+	gulp.watch("../**/*.php", ['php'])
 });
